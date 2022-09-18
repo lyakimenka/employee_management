711f9f0903c3f57b6758b91f15fd2ecf532cd34b
lyakimenka <70149789+lyakimenka@users.noreply.github.com>
2022-09-18 17:33:11 -0500

    Create TaxPayment.java

---
+++ TaxPayment.java
@@ -0,0 +1,11 @@
+public class TaxPayment
+{
+  double FederalTax1 = 0.1;
+  double FederalTax2 = 0.15;
+  double FederalTax3 = 0.2;
+  double FederalTax4 = 0.25;
+  double stateTaxIL = 0.05;
+  double stateTaxWI = 0.03;
+  double FicaTax = 0.0765;
+  
+}
